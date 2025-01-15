b343b0c96ea16cefb02774450699826bdb9cf400
mahsasalavati <salavatimahsa73@gmail.com>
2025-01-16 00:46:43 +0330

    Update index.jsx

--- src/Pages/Home/Discountproducts/index.jsx
+++ src/Pages/Home/Discountproducts/index.jsx
@@ -4,26 +4,51 @@ import Link from "@mui/material/Link";
 
 export default function Discountproducts() {
   return (
-    <>
+    <Stack
+      sx={{ display: "flex", flexDirection: "row", flexWrap: "wrap", gap: 2, margin:5}}
+    >
       <Stack
         sx={{
           width: 400,
-          height:200,
+          height: 200,
           borderRadius: 3,
-          margin:'auto',
           background:
             "linear-gradient(315deg, rgba(195,166,34,1) 0%, rgba(253,45,95,1) 100%)",
-            display:'flex',
-            flexDirection:'row'
+          display: "flex",
+          flexDirection: "row",
         }}
       >
-        <Box component={"img"} sx={{width:'30%', overflow:'hidden'}}></Box>
-        <Stack sx={{display:'flex', justifyContent:'center'}}>
-        <Box component={"h3"} sx={{paddingLeft:5}}>UP TO 60% DISCOUNT</Box>
-        <Link sx={{paddingLeft:12}} href="#">Shop now</Link>
+        <Box component={"img"} sx={{ width: "30%", overflow: "hidden" }}></Box>
+        <Stack sx={{ display: "flex", justifyContent: "center" }}>
+          <Box component={"h3"} sx={{ paddingLeft: 5 }}>
+            UP TO 60% DISCOUNT
+          </Box>
+          <Link sx={{ paddingLeft: 12 }} href="#">
+            Shop now
+          </Link>
         </Stack>
-        
       </Stack>
-    </>
+      <Stack
+        sx={{
+          width: 400,
+          height: 200,
+          borderRadius: 3,
+          background:
+            "linear-gradient(315deg, rgba(195,166,34,1) 0%, rgba(253,45,95,1) 100%)",
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        <Box component={"img"} sx={{ width: "30%", overflow: "hidden" }}></Box>
+        <Stack sx={{ display: "flex", justifyContent: "center" }}>
+          <Box component={"h3"} sx={{ paddingLeft: 5 }}>
+            UP TO 60% DISCOUNT
+          </Box>
+          <Link sx={{ paddingLeft: 12 }} href="#">
+            Shop now
+          </Link>
+        </Stack>
+      </Stack>
+    </Stack>
   );
 }
